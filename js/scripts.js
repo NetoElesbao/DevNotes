2a5c2368f2fd26c8f66c50fa319d800ce6b2d4fb
Neto Elesbão <josenetu32@gmail.com>
2024-09-20 20:14:25 -0300

    V1.5

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,7 @@ const addNoteBtn = document.querySelector(".add-note");
 
 // Adiciona nota
 function addNote() {
-  //   console.log("Funcionou!");
-  //   debugger;
+  const notes = getNotes();
 
   const noteObject = {
     id: generateId(),
@@ -18,9 +17,17 @@ function addNote() {
     fixed: false,
   };
 
+  // Cria a nota
   const noteElement = createNote(noteObject.id, noteObject.content);
-  //   console.log(noteElement);
+
+  //Adiciona a nota para ser exibida
   notesContainer.appendChild(noteElement);
+
+  notes.push(noteObject);
+
+  saveNotesInLocalStorage(notes); // Tentar resolver o porquê as notas estão sendo salvas vazias
+
+  noteContent.value = "";
 }
 
 // Gera o Id da nota
@@ -29,32 +36,101 @@ function generateId() {
 }
 
 // Cria a nota
-function createNote(id, content, fixed) {
+function createNote(id, content, fixed = false) {
   const div = document.createElement("div");
   div.classList.add("note");
 
   //   Forma 1
-  div.innerHTML = `<textarea placeholder="Adicione algum texto">${content}</textarea>
+  //   div.innerHTML = `<textarea placeholder="Adicione algum texto">${content}</textarea>
 
-          <i class="bi bi-pin"></i>
+  //           <i class="bi bi-pin"></i>
 
-          <i class="bi bi-x-lg"></i>
+  //           <i class="bi bi-x-lg"></i>
 
-          <i class="bi bi-file-earmark-plus"></i>`;
+  //           <i class="bi bi-file-earmark-plus"></i>`;
 
-  //   // Forma 2
-  //   const textarea = document.createElement("textarea");
+  // Forma 2
 
-  //   textarea.value = content;
+  if (fixed) {
+    div.classList.add("fixed");
+  }
 
-  //   textarea.placeholder = "Adicione algum texto";
+  const textarea = document.createElement("textarea");
+  textarea.value = content;
+  textarea.placeholder = "Adicione algum texto";
+  div.appendChild(textarea);
 
-  //   div.appendChild(textarea);
+  const pinBtn = document.createElement("i");
+  pinBtn.classList.add(...["bi"], ["bi-pin"]);
+  div.appendChild(pinBtn);
 
-  //   const iconOne =
+  const deleteBtn = document.createElement("i");
+  deleteBtn.classList.add(...["bi"], ["bi-x-lg"]);
+  div.appendChild(deleteBtn);
+
+  const copyBtn = document.createElement("i");
+  copyBtn.classList.add(...["bi"], ["bi-file-earmark-plus"]);
+  div.appendChild(copyBtn);
+
+  //   Evento de troca do atributo fixed da nota
+  div.querySelector(".bi-pin").addEventListener("click", () => {
+    toggleFixNote(id);
+  });
 
   return div;
 }
 
+function toggleFixNote(id) {
+  const notes = getNotes();
+  //   debugger;
+  const noteTarget = notes.filter((note) => note.id === id)[0];
+
+  noteTarget.fixed = !noteTarget.fixed;
+
+  saveNotesInLocalStorage(notes);
+
+  loadNotes();
+}
+
+// Salva a nota no localStorage
+function saveNotesInLocalStorage(notes) {
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
+
+// localStorage--------------
+
+// Carrega as notas dp localStorage caso existam, caso não, cria um array vazio.
+function getNotes() {
+  const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+
+  const orderedNotes = notes.sort((a, b) => (b.fixed > a.fixed ? 10 : -10));
+
+  return orderedNotes;
+}
+
+function loadNotes() {
+  cleanNotes();
+
+  getNotes().forEach((note) => {
+    console.log("Entrou");
+
+    const noteElement = createNote(note.id, note.content, note.fixed);
+
+    notesContainer.appendChild(noteElement);
+  });
+}
+
+function cleanNotes() {
+  notesContainer.replaceChildren([]);
+}
+
 // Eventos--------------------------------------------------------------------------------------------------------------------
-addNoteBtn.addEventListener("click", () => addNote());
+
+// Adiciona nota
+addNoteBtn.addEventListener("click", () => {
+  //   debugger;
+  addNote();
+});
+
+// Inicialização
+loadNotes();
